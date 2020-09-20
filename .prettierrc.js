96cfb6b1670c0cd69b8fd07ac814fd11ccc5f63a
Mohammad Hasan <mohamad.500.alhassan@gmail.com>
2020-09-20 04:16:04 -0400

    Dota Dashboard Initialization

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = 
+{
+  "printWidth": 100,
+  "bracketSpacing": false,
+  "jsxBracketSameLine": true,
+  "singleQuote": true,
+  "trailingComma": "all",
+  "quoteProps": "preserve"
+}
